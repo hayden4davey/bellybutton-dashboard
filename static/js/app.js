25b2079dce2ba4dba0059e236f2d2158402250ad
hayden4davey <hayden4davey@gmail.com>
2020-01-23 14:01:12 -0500

    add funcs

--- static/js/app.js
+++ static/js/app.js
@@ -1,22 +1,42 @@
 // Assign variable to json file
 var sampleData = "../../samples.json";
 
+// Select dropdown
+var dropdown = d3.select("#selDataset");
+
 // Create init function
 function init(){
 
     // Select dropdown
-    var dropdown = d3.select("#selDataset")
+    var dropdown = d3.select("#selDataset");
 
     // Read dataset and send list of names to dropdown menu
-    d3.json(sampleData).then((data)=>{
+    d3.json(sampleData).then((data) => {
         console.log(data);
-        data.names.map((name)=>{
+        data.names.map((name) => {
             dropdown.append("option").text(name).property("value");
         })
-        
-
 
     })
 }
+
+// Change function
+function optionChanged(id){
+    getData(id);
+
+}
+
+
+// Function to pull data based on id
+function getData(id){
+    console.log(id)
+
+   
+    
+    
+}
+
+
+
 
 init();
